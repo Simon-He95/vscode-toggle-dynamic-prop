762133c93efa3bc73319de6fdf82e912fc9a298b
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-07-29 15:03:43 +0800

    chore: refactor type

--- src/toggleExport.ts
+++ src/toggleExport.ts
@@ -1,16 +1,16 @@
 import type { getSelection } from '@vscode-use/utils'
 import { createPosition, createRange, updateText } from '@vscode-use/utils'
 
-export function toggleExport(selection: ReturnType<typeof getSelection>) {
-  const match = selection!.lineText.match(/^\s*export\s*(?:default\s*)?/)
+export function toggleExport(selection: NonNullable<ReturnType<typeof getSelection>>) {
+  const match = selection.lineText.match(/^\s*export\s*(?:default\s*)?/)
   if (match) {
     updateText((edit) => {
-      edit.delete(createRange(selection!.line, 0, selection!.line, match.index! + match[0].length))
+      edit.delete(createRange(selection.line, 0, selection!.line, match.index! + match[0].length))
     })
   }
   else {
     updateText((edit) => {
-      edit.insert(createPosition(selection!.line, 0), 'export ')
+      edit.insert(createPosition(selection.line, 0), 'export ')
     })
   }
 }
