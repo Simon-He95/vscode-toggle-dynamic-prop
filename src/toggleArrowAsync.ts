292344c4329c47ea38940bbc34a6bc73ba7543b5
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-05-21 11:26:03 +0800

    fix: add match check in toggleAsync and toggleArrowAsync functions

--- src/toggleArrowAsync.ts
+++ src/toggleArrowAsync.ts
@@ -4,6 +4,8 @@ import { createPosition, createRange, updateText } from '@vscode-use/utils'
 export function toggleArrowAsync(selection: NonNullable<ReturnType<typeof getSelection>>) {
   const lineText = selection.lineText
   const match = lineText.match(/((?:export|export default)?(?:\s+(?:const|let|var))?\s*[\w<>]+(?::|\s*=)?\s*\w+\()(async\s+)?(\([^)]*\)\s+)=>/)!
+  if (!match)
+    return
   if (match[2]) {
     updateText((edit) => {
       edit.delete(createRange(selection.line, match.index! + match[1].length, selection!.line, match.index! + match[1].length + match[2].length))
--- src/toggleAsync.ts
+++ src/toggleAsync.ts
@@ -4,6 +4,8 @@ import { createPosition, createRange, updateText } from '@vscode-use/utils'
 export function toggleAsync(selection: NonNullable<ReturnType<typeof getSelection>>) {
   const lineText = selection.lineText
   const match = lineText.match(/((?:export|export default)?\s*)(async\s+)?(function\s+)?[\w<>]+\s*\(/)!
+  if (!match)
+    return
   if (match[2]) {
     updateText((edit) => {
       edit.delete(createRange(selection.line, match.index! + match[1].length, selection!.line, match.index! + match[1].length + match[2].length))
